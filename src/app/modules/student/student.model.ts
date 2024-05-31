0c6cb00ede33d4ff2c600dcd9e9a3b6f474df715
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-05-31 09:50:02 +0600

    user model created

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+import { TUser } from "./student.interface";
+
+//create schema for user
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "faculty", "student"],
+    },
+    status: {
+      type: String,
+      enum: ["in-progress", "blocked"],
+      default: "in-progress",
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model<TUser>("User", userSchema);
