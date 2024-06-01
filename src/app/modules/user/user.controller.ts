34811b827c37559a0b9f52ed9091e27fef70dd88
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-06-01 10:29:44 +0600

    catch async handler utils created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,35 +3,20 @@ import { UserValidation } from "./user.validation";
 import { UserServices } from "./user.service";
 import sendResponse from "../../utils/sendResponse";
 import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { student: studentData, password } = req.body;
+const createStudent = catchAsync(async (req, res) => {
+  const { student: studentData, password } = req.body;
 
-    //const zodParseData = UserValidation.userValidationSchema.parse(userData);
-    const result = await UserServices.createStudentIntoDB(
-      studentData,
-      password
-    );
-    // res.status(201).json({
-    //   success: true,
-    //   message: "student created successfully",
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "student created successfully",
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  //const zodParseData = UserValidation.userValidationSchema.parse(userData);
+  const result = await UserServices.createStudentIntoDB(studentData, password);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "student created successfully",
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
