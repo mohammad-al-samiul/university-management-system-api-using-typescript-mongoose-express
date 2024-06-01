a8a9eb3f22b3d6447e1c40e60203ee4689f5e64a
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-06-01 11:16:07 +0600

    create validate request middleware

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,4 +1,3 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
 import { StudentServices } from "./student.service";
 import httpStatus from "http-status";
 import sendResponse from "../../utils/sendResponse";
