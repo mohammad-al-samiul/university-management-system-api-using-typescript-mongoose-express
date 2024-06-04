9f2c001bace9929e97db402466708cded888713a
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-06-04 10:16:06 +0600

    academic faculty schema and model, interface created

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+import { TAcademicFaculty } from "./academicFaculty.interface";
+
+const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const AcademicFaculty = mongoose.model(
+  "AcademicFaculty",
+  academicFacultySchema
+);
