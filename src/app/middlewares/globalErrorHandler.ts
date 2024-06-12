922b3b03d1b2cc276912835ab824fd6683a0a570
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-06-12 16:04:08 +0600

    handle validation error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,12 +1,14 @@
 import { ErrorRequestHandler } from "express";
-import { TErrorSource } from "../interface/error";
+import { TErrorSources } from "../interface/error";
 import { ZodError } from "zod";
 import handleZodError from "../errors/handleZodError";
+import config from "../config";
+import handleValidationError from "../errors/handleValidationError";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = 500;
   let message = "Something Went Wrong!";
-  let errorSources: TErrorSource = [
+  let errorSources: TErrorSources = [
     {
       path: "",
       message: "Something Went Wrong!",
@@ -18,12 +20,18 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
-
   res.status(statusCode).json({
     success: false,
     message,
     errorSources,
+    stack: config.node_env === "developement" ? err?.stack : null,
+    err,
   });
 };
 
