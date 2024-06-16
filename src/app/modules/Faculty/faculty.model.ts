785d53a3e490a9df5c267d7cdaca1c3d220038c0
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-06-16 10:57:17 +0600

    faculty model created

--- src/app/modules/Faculty/faculty.model.ts
+++ src/app/modules/Faculty/faculty.model.ts
@@ -1,5 +1,6 @@
-import { Schema, Types } from "mongoose";
+import mongoose, { Schema, Types } from "mongoose";
 import { FacultyModel, TFaculty, TFacultyUserName } from "./faculty.interface";
+import { BloodGroup, Gender } from "./faculty.constant";
 
 const userNameSchema = new Schema<TFacultyUserName>({
   firstName: {
@@ -15,23 +16,112 @@ const userNameSchema = new Schema<TFacultyUserName>({
   },
 });
 
-const facultySchema = new Schema<TFaculty, FacultyModel>({
-  id: {
-    type: String,
-    required: [true, "ID is required"],
-    unique: true,
+const facultySchema = new Schema<TFaculty, FacultyModel>(
+  {
+    id: {
+      type: String,
+      required: [true, "ID is required"],
+      unique: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, "User ID is required"],
+      ref: "User",
+    },
+    designation: {
+      type: String,
+      required: [true, "Designation is required"],
+    },
+    name: {
+      type: userNameSchema,
+      required: [true, "Name is requierd"],
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: Gender,
+        message: `{VALUE} is not a valid`,
+      },
+      required: [true, "Gender is requierd"],
+    },
+    dateOfBirth: { type: Date },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    contactNo: { type: String, required: [true, "Contact number is required"] },
+    emergencyContactNo: {
+      type: String,
+      required: [true, "Emergency contact number is required"],
+    },
+    bloodGroup: {
+      type: String,
+      enum: {
+        values: BloodGroup,
+        message: `{VALUE} is not valid`,
+      },
+    },
+    presentAddress: {
+      type: String,
+      required: [true, "Present address is required"],
+    },
+    permanentAddress: {
+      type: String,
+      required: [true, "Permanent address is required"],
+    },
+    profileImg: { type: String },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      required: [true, "User id is required"],
+      ref: "User",
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    required: [true, "User ID is required"],
-    ref: "User",
-  },
-  designation: {
-    type: String,
-    required: [true, "Designation is required"],
-  },
-  name: {
-    type: userNameSchema,
-    required: [true, "Name is requierd"],
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// generating full name
+facultySchema.virtual("fullName").get(function () {
+  return (
+    this?.name?.firstName +
+    "" +
+    this?.name?.middleName +
+    "" +
+    this?.name?.lastName
+  );
 });
+
+// filter out deleted documents
+facultySchema.pre("find", function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+facultySchema.pre("findOne", function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+facultySchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
+  next();
+});
+
+//checking if user is already exist!
+facultySchema.statics.isUserExists = async function (id: string) {
+  const existingUser = await Faculty.findOne({ id });
+  return existingUser;
+};
+
+export const Faculty = mongoose.model<TFaculty, FacultyModel>(
+  "Faculty",
+  facultySchema
+);
